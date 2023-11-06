94f1cf6f6bccec5e17ffd3c4a75dc6770b8a983c
Jacob Jose <jjose14@asu.edu>
2023-11-06 16:51:03 -0700

    20231106_1650 v1.0 Added different key lengths to KeyGenerator.

--- keyGenerator/src/main/java/com/jacob/KeyGenerator.java
+++ keyGenerator/src/main/java/com/jacob/KeyGenerator.java
@@ -5,25 +5,59 @@ import java.util.Base64;
 
 public class KeyGenerator {
 
-    private static final String MSG_NEW_LINES = "\n\n";
-    private static final String MSG_OUTPUT_PREFIX = "Key Generated is: ";
+    private static final String MSG_NEW_LINE = "\n";
+    private static final String MSG_OUTPUT_PREFIX = "Format-> Length (in bits): Key";
+    private static final String MSG_DELIMITER = " : ";
+    private static final int [] ARRAY_KEY_LENGTH_IN_BITS = new int [] { 16, 32, 64, 128, 256, 512, 1024, 2048 };
 
-    public static String GenerateKey () {
-        int keyLength = 256 / 8; // 256 bits
-        byte[] secretKey = new byte[keyLength];
+    /**
+     * Generates a key.
+     * @param keyLengthInBits length of the key in bits.
+     * @return Return a String containing the keys.
+     */
+    private static String generateKey(int keyLengthInBits) {
+        byte[] secretKey = new byte[keyLengthInBits];
         new SecureRandom().nextBytes(secretKey);
         return Base64.getEncoder().encodeToString(secretKey);
     }
 
-    public static void printOutput(String output) {
-        System.out.print(MSG_NEW_LINES);
-        System.out.print(MSG_OUTPUT_PREFIX);
-        System.out.print(output);
-        System.out.print(MSG_NEW_LINES);
+    /**
+     * Appends duplicates of the given String into a new String and returns the new String.
+     * @param stringOriginal: String which is to be duplicated.
+     * @param count: number of duplicates.
+     * @return : String where duplicates are concatenated.
+     */
+    private static String getStringDuplicatesConcatenated(String stringOriginal, int count) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while(count-->0) {
+            stringBuilder.append(stringOriginal);
+        }
 
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Generates keys of different lengths from an array which contains the key lengths.
+     * @param arrayKeyLengthsInBits: Integer array containing different key sizes.
+     * @return : String containing keys of the lengths specified in the array.
+     */
+    public static String generateAndPrintFromKeysArray(int [] arrayKeyLengthsInBits) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append(getStringDuplicatesConcatenated(MSG_NEW_LINE, 1));
+        stringBuilder.append(MSG_OUTPUT_PREFIX);
+        stringBuilder.append(getStringDuplicatesConcatenated(MSG_NEW_LINE, 2));
+
+        for (int keySize: arrayKeyLengthsInBits) {
+            stringBuilder.append(keySize);
+            stringBuilder.append(MSG_DELIMITER);
+            stringBuilder.append(generateKey(keySize));
+            stringBuilder.append(getStringDuplicatesConcatenated(MSG_NEW_LINE, 2));
+        }
+        return stringBuilder.toString();
     }
 
     public static void main (String [] args) {
-        KeyGenerator.printOutput(KeyGenerator.GenerateKey());
+        System.out.println(KeyGenerator.generateAndPrintFromKeysArray(ARRAY_KEY_LENGTH_IN_BITS));
     }
 }
